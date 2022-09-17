9a5cfca34e6f81bd91861dc22ac43b57f1bfbbb9
SiansandaTamara <tamarasiansanda18@gmail.com>
2022-09-17 11:32:12 +0200

    fix errors

--- db/migrate/20220917092241_jwt_denylist.rb
+++ db/migrate/20220917092241_jwt_denylist.rb
@@ -1,4 +1,4 @@
-class CreateJwtDenylist < ActiveRecord::Migration[7.0]
+class JwtDenylist < ActiveRecord::Migration[7.0]
   def change
     create_table :jwt_denylist do |t|
       t.string :jti, null: false
